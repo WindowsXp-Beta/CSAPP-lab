0ba68ee6bfeffd451483020146fb71b69489b90e
XPdeUbuntu <william.xp.wei@gmail.com>
2021-05-22 20:58:12 -0700

    Encounter timeout problem which cannot solve by me

--- proxy/proxy.c
+++ proxy/proxy.c
@@ -14,18 +14,127 @@
 int parse_uri(char *uri, char *target_addr, char *path, char *port);
 void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, size_t size);
 
+ssize_t Rio_readlineb_w(rio_t *rp, void *usrbuf, size_t maxlen)
+{
+    ssize_t rc;
+
+    if ((rc = rio_readlineb(rp, usrbuf, maxlen)) < 0)
+    {
+        fprintf(stderr, "Rio_readlineb error: %s\n", strerror(errno));
+        return 0;
+    }
+    return rc;
+}
+
+ssize_t Rio_readnb_w(rio_t *rp, void *usrbuf, size_t n)
+{
+    ssize_t rc;
+
+    if ((rc = rio_readnb(rp, usrbuf, n)) < 0)
+    {
+        fprintf(stderr, "Rio_readnb error: %s\n", strerror(errno));
+        return 0;
+    }
+    return rc;
+}
+
+void Rio_writen_w(int fd, void *usrbuf, size_t n)
+{
+    if (rio_writen(fd, usrbuf, n) != n)
+    {
+        fprintf(stderr, "Rio_writen error: %s\n", strerror(errno));
+        return;
+    }
+}
 
 /*
  * main - Main routine for the proxy program
  */
 int main(int argc, char **argv)
 {
+    /* 作为server端 */
+    int listenfd, connfd;
+    /* 作为client端 */
+    int clientfd;
+
+    char hostname[MAXLINE], port[MAXLINE], path[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char buf_server[MAXLINE], buf_client[MAXLINE];
+    char rep_from_server[MAXLINE], rep_head_to_client[MAXLINE], rep_body_to_client[MAXLINE];
+    char logString[MAXLINE];
+
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    rio_t rio_client, rio_server;
+    size_t size;//size return from end server
+
     /* Check arguments */
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
         exit(0);
     }
+    
+    listenfd = Open_listenfd(argv[1]);
+    while(1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
 
+        /* 处理http请求 */
+        Rio_readinitb(&rio_client, connfd);
+        Rio_readlineb_w(&rio_client, buf_client, MAXLINE);
+        sscanf(buf_client, "%s %s %s", method, uri, version);
+        
+        /* close it from telnet, for debug */
+        if(strcmp(method, "STOP") == 0) {
+            Close(connfd);
+            break;
+        }
+
+        if (parse_uri(uri, hostname, path, port) == -1) {
+            fprintf(stderr, "Wrong uri\n");
+            Close(connfd);
+            continue;
+        }
+
+        /* 作为client向url发送请求 */
+        clientfd = Open_clientfd(hostname, port);
+        /* build HTTP request */
+        sprintf(buf_server, "%s /%s %s\r\n", method, path, "HTTP/1.1");
+
+        do {
+            Rio_readlineb_w(&rio_client, buf_client, MAXLINE);
+            sprintf(buf_server, "%s%s", buf_server, buf_client);
+        } while(strcmp(buf_client, "\r\n"));
+        // printf("request header is\n%s", buf_server);
+        /* send Http request */
+        Rio_writen_w(clientfd, buf_server, strlen(buf_server));
+
+        /* receive reponse header from server and set reponse header to client */
+        Rio_readinitb(&rio_server, clientfd);
+        do {
+            Rio_readlineb_w(&rio_server, rep_from_server, MAXLINE);
+            if(strncasecmp(rep_from_server, "Content-Length", 14) == 0) {
+                size = strtol(rep_from_server + 16, NULL, 10);
+                // printf("%lu\n", size);
+            }
+            sprintf(rep_head_to_client, "%s%s", rep_head_to_client, rep_from_server);
+        } while(strcmp(rep_from_server, "\r\n"));
+        format_log_entry(logString, &clientaddr, uri, size);
+        printf("%s\n", logString);
+        /* sent response header to client */
+        Rio_writen_w(connfd, rep_head_to_client, strlen(rep_head_to_client));
+
+        /* sent response body to client */
+        while(size > 0) {
+            size -= Rio_readlineb_w(&rio_server, rep_from_server, MAXLINE);
+            Rio_writen_w(connfd, rep_from_server, strlen(rep_from_server));
+        }
+
+
+        Close(clientfd);
+        Close(connfd);
+    }
     exit(0);
 }
 
